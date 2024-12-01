4c40cc171fb31cfea03db00c3e1dece8b02dd57b
Junseong Park <39112954+jsparkdev@users.noreply.github.com>
2024-12-01 16:08:00 +0900

    shadcn

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,21 +1,21 @@
-import type { Route } from "../routes/+types/_index";
+import type { Route } from "./+types/_index";
 
 export async function loader({ context }: Route.LoaderArgs) {
-  const { title, description } = context.cloudflare.env;
-  return { title, description };
+	const { title, description } = context.cloudflare.env;
+	return { title, description };
 }
 
 export function meta({ data }: Route.MetaArgs) {
-  const { title, description } = data;
-  return [{ title }, { name: "description", content: description }];
+	const { title, description } = data;
+	return [{ title }, { name: "description", content: description }];
 }
 
 export default function Index({ loaderData }: Route.ComponentProps) {
-  const { title, description } = loaderData;
-  return (
-    <main className="w-[600px] mx-auto flex flex-col justify-center h-screen">
-      <h1 className="font-bold text-4xl text-center">{title}</h1>
-      <p className="text-center">{description}</p>
-    </main>
-  );
+	const { title, description } = loaderData;
+	return (
+		<main className="w-[600px] mx-auto flex flex-col justify-center h-screen">
+			<h1 className="font-bold text-4xl text-center">{title}</h1>
+			<p className="text-center">{description}</p>
+		</main>
+	);
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,23 @@
 import type { Config } from "tailwindcss";
 
 export default {
+	darkMode: ["class"],
 	content: ["./app/**/*.tsx"],
 	theme: {
-		fontFamily: {
-			sans: ["Geist", "sans-serif"],
-		},
-		height: {
-			screen: "100dvh",
+		extend: {
+			fontFamily: {
+				sans: ["Geist", "sans-serif"],
+			},
+			height: {
+				screen: "100dvh",
+			},
+			borderRadius: {
+				lg: "var(--radius)",
+				md: "calc(var(--radius) - 2px)",
+				sm: "calc(var(--radius) - 4px)",
+			},
+			colors: {},
 		},
 	},
-	plugins: [],
+	plugins: [require("tailwindcss-animate")],
 } satisfies Config;
