344ad765f2763e459d8b3e0bae94493622a146f9
Junseong Park <39112954+jsparkdev@users.noreply.github.com>
2024-12-01 19:03:15 +0900

    update

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# React Router + Cloudflare Pages
+# React Router + Cloudflare Pages Template
 
-A template for building a React Router app with Cloudflare Pages.
+A minimal template for building a React Router App with Cloudflare Pages.
+
+- [Vite](https://vite.dev/) + [React Router v7](https://reactrouter.com/)
+- [shadcn/ui](https://ui.shadcn.com/)
+- [React 19](https://react.dev)
+- [Biome](https://biomejs.dev/)
+- Deploy to [Cloudflare Pages](https://pages.cloudflare.com/)
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,21 +1,47 @@
+import { Button } from "~/components/ui/button";
 import type { Route } from "./+types/_index";
+import { Github } from "lucide-react";
 
 export async function loader({ context }: Route.LoaderArgs) {
-	const { title, description } = context.cloudflare.env;
-	return { title, description };
+  const { title, description } = context.cloudflare.env;
+  return { title, description };
 }
 
 export function meta({ data }: Route.MetaArgs) {
-	const { title, description } = data;
-	return [{ title }, { name: "description", content: description }];
+  const { title, description } = data;
+  return [{ title }, { name: "description", content: description }];
 }
 
 export default function Index({ loaderData }: Route.ComponentProps) {
-	const { title, description } = loaderData;
-	return (
-		<main className="w-[600px] mx-auto flex flex-col justify-center h-screen">
-			<h1 className="font-bold text-4xl text-center">{title}</h1>
-			<p className="text-center">{description}</p>
-		</main>
-	);
+  const { title, description } = loaderData;
+  return (
+    <main className="flex flex-col justify-center h-screen gap-4">
+      <h1 className="font-bold text-4xl text-center text-balance">{title}</h1>
+      <p className="text-center text-balance">{description}</p>
+      <div className="flex justify-center gap-4 flex-wrap">
+        <Button asChild className="w-[200px]">
+          <a
+            href="https://github.com/new?template_name=react-router-cloudflare-pages-template&template_owner=jsparkdev"
+            target="_blank"
+            rel="noreferrer"
+            title="Use this template"
+          >
+            <Github />
+            Use this template
+          </a>
+        </Button>
+        <Button asChild className="w-[200px]">
+          <a
+            href="https://github.com/jsparkdev/react-router-cloudflare-pages-template"
+            target="_blank"
+            rel="noreferrer"
+            title="Github Repository"
+          >
+            <Github />
+            Github Repository
+          </a>
+        </Button>
+      </div>
+    </main>
+  );
 }
